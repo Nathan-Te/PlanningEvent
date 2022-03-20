5d4d7d8e3508b03fc4732eb93e2c35dac3bbc084
Mathilde <Mathilde.Mottay.Etu@univ-lemans.fr>
2022-03-20 17:49:44 +0100

    Swipe écran pour favoris

--- app/src/main/java/com/example/planningevent/EventsManager.java
+++ app/src/main/java/com/example/planningevent/EventsManager.java
@@ -1,11 +1,14 @@
 package com.example.planningevent;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
@@ -21,16 +24,7 @@ public class EventsManager extends Activity {
     RecyclerView rv;
     List<Evenement> myEvents = new ArrayList<>();
     MyAdapter adapter;
-    Evenement data_ev;
     EventsManager objet = this;
-
-    public Evenement getData(){
-        return this.data_ev;
-    }
-
-    public void setData(Evenement s){
-        this.data_ev = s;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +39,7 @@ public class EventsManager extends Activity {
 
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 myEvents.clear();
@@ -71,20 +65,13 @@ public class EventsManager extends Activity {
             }
         });
 
+
+
         // Back home
         FloatingActionButton btnHome = (FloatingActionButton) findViewById(R.id.btnHome);
         btnHome.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent MainActivityIntent = new Intent(EventsManager.this, MainActivity.class);
-                startActivity(MainActivityIntent);
-            }
-        });
-
-        // Favorite
-        FloatingActionButton btnFavorite = (FloatingActionButton) findViewById(R.id.btnFavorite);
-        btnFavorite.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent MainActivityIntent = new Intent(EventsManager.this, EventsFavorite.class);
                 startActivity(MainActivityIntent);
             }
         });
@@ -95,6 +82,97 @@ public class EventsManager extends Activity {
             public void onClick(View v) {
                 Intent EventAdderIntent = new Intent(EventsManager.this, EventAdder.class);
                 startActivity(EventAdderIntent);
+            }
+        });
+
+        rv.setOnTouchListener(new OnSwipeTouchListener(EventsManager.this) {
+            public void onSwipeLeft() {
+                // Actualisation du titre
+                TextView monTitre = findViewById(R.id.monTitre);
+                monTitre.setText("My favorites");
+
+                // Changement couleur indicateur
+                ImageView viewFavorites = findViewById(R.id.viewFavorites);
+                ImageView viewAllEvents = findViewById(R.id.viewAllEvents);
+                viewFavorites.setImageDrawable(viewFavorites.getResources().getDrawable(R.drawable.blue_circle));
+                viewAllEvents.setImageDrawable(viewFavorites.getResources().getDrawable(R.drawable.circle_gray));
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance("https://planning-event-default-rtdb.europe-west1.firebasedatabase.app/");
+                DatabaseReference myRef = database.getReference("events");
+
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+                        myEvents.clear();
+                        for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                            if((boolean) ds.child("/favorite").getValue() == true){
+                                String event_name = String.valueOf(ds.child("/name").getValue());
+                                String date = String.valueOf(ds.child("/date").getValue());
+                                long hour = (long) ds.child("/hour").getValue();
+                                long minute = (long) ds.child("/minute").getValue();
+                                long nb_people = (long) ds.child("/nb_people").getValue();
+                                long id = (long) ds.child("/id").getValue();
+
+                                Evenement event = new  Evenement(event_name,(int) hour, (int) minute, (int) nb_people, date);
+                                event.setId((int) id);
+
+                                myEvents.add(event);
+                            }
+                        }
+                        adapter = new MyAdapter(objet,myEvents);
+                        rv.setAdapter(adapter);
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                    }
+                });
+            }
+
+            public void onSwipeRight() {
+                // Actualisation du titre
+                TextView monTitre = findViewById(R.id.monTitre);
+                monTitre.setText("My events");
+
+                // Changement couleur indicateur
+                ImageView viewFavorites = findViewById(R.id.viewFavorites);
+                ImageView viewAllEvents = findViewById(R.id.viewAllEvents);
+                viewFavorites.setImageDrawable(viewFavorites.getResources().getDrawable(R.drawable.circle_gray));
+                viewAllEvents.setImageDrawable(viewFavorites.getResources().getDrawable(R.drawable.blue_circle));
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance("https://planning-event-default-rtdb.europe-west1.firebasedatabase.app/");
+                DatabaseReference myRef = database.getReference("events");
+
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+                        myEvents.clear();
+                        for(DataSnapshot ds : dataSnapshot.getChildren()) {
+
+                            String event_name = String.valueOf(ds.child("/name").getValue());
+                            String date = String.valueOf(ds.child("/date").getValue());
+                            long hour = (long) ds.child("/hour").getValue();
+                            long minute = (long) ds.child("/minute").getValue();
+                            long nb_people = (long) ds.child("/nb_people").getValue();
+                            long id = (long) ds.child("/id").getValue();
+
+                            Evenement event = new  Evenement(event_name,(int) hour, (int) minute, (int) nb_people, date);
+                            event.setId((int) id);
+
+                            myEvents.add(event);
+                        }
+                        adapter = new MyAdapter(objet,myEvents);
+                        rv.setAdapter(adapter);
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                    }
+                });
             }
         });
     }
